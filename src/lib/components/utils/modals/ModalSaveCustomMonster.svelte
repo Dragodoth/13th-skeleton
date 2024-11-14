a2d64abb5f83efd1e3a9f5cf53ca1916a25840fb
Viktor <xsensysx@gmail.com>
2024-11-14 19:31:42 +0100

    more work on custom monsters

--- src/lib/components/utils/modals/ModalSaveCustomMonster.svelte
+++ src/lib/components/utils/modals/ModalSaveCustomMonster.svelte
@@ -18,7 +18,7 @@
 
     // Form Data
     let formData: Monster = $state({
-        name: 'Skeleton Battle',
+        name: "Skeleton",
         ac: 0,
         basename: "",
         hp: 0,
@@ -29,63 +29,70 @@
         md: 0,
         pd: 0,
         preview: "",
-        role: "",
-        size: "",
-        type: "",
-        actions: [{
-            name: "",
-            desc: "",
-            traits: [{
-                name: "",
-                desc: "",
-            }],
-        }],
-        nastier_traits: [{
-            name: "",
-            desc: "",
-        }],
-        triggered_actions: [{
-            name: "",
-            desc: "",
-            traits: [{
-                name: "",
-                desc: "",
-            }],
-        }]
+        role: "troop",
+        size: "normal",
+        type: "humanoid",
     });
 
-    function handleAddFormClick(key: string, type: string) {
-        const sampleTrait: Trait = {
-            name: "",
-            desc: "",
-        };
-        const sampleAction: Action = {
-            name: "",
-            desc: "",
-            traits: [sampleTrait],
-        };
+    function handleAddFormClick(key: string, actionTrait?: boolean, index?: number) {
+        const sampleTrait: Trait = { name: "", desc: "" };
+        const sampleAction: Action = { name: "", desc: "", traits: [sampleTrait] };
 
-        if (!(key in formData)) {
-            formData = { ...formData, [key]: type === "trait" ? [sampleTrait] : [sampleAction] };
+        let updatedFormData = { ...formData };
+
+        if (actionTrait && (key === "actions" || key === "triggered_actions") && index) {
+            const actionsCopy = [...(updatedFormData[key] as Action[])];
+            const targetAction = { ...actionsCopy[index] };
+
+            targetAction.traits = [...targetAction.traits as Trait[], sampleTrait];
+            actionsCopy[index] = targetAction;
+
+            updatedFormData[key] = actionsCopy;
+        } else {
+            if (!(key in updatedFormData)) {
+                updatedFormData[key] = key === "traits" || key === "nastier_traits" ? [sampleTrait] : [sampleAction];
+            } else {
+                if (key === "traits" || key === "nastier_traits") {
+                    updatedFormData[key] = [...(updatedFormData[key] as Trait[]), sampleTrait];
+                }
+
+                if (key === "actions" || key === "triggered_actions") {
+                    updatedFormData[key] = [...(updatedFormData[key] as Action[]), sampleAction];
+                }
+            }
         }
 
-        if (type === "trait" && (key === "traits" || key === "nastier_traits")) {
-            formData = {
-                ...formData,
-                [key]: [...(formData[key] as Trait[]), sampleTrait]
-            };
+        formData = updatedFormData;
+    }
+
+    function handleRemoveFormClick(key: string, actionTrait?: boolean, index?: number | null, traitIndex?: number ) {
+        let updatedFormData = { ...formData };
+
+        if (actionTrait && (key === "actions" || key === "triggered_actions") && index && traitIndex) {
+            const actionsCopy = [...(updatedFormData[key] as Action[])];
+            const targetAction = { ...actionsCopy[index] };
+
+            targetAction.traits = (targetAction.traits as Trait[]).filter((_, i) => i !== traitIndex);
+            actionsCopy[index] = targetAction;
+
+            updatedFormData[key] = actionsCopy;
+        } else {
+            if (key in updatedFormData && index) {
+                if (key === "traits" || key === "nastier_traits") {
+                    updatedFormData[key] = (updatedFormData[key] as Trait[]).filter((_, i) => i !== index);
+                }
+
+                if (key === "actions" || key === "triggered_actions") {
+                    updatedFormData[key] = (updatedFormData[key] as Action[]).filter((_, i) => i !== index);
+                }
+            }
         }
 
-        if (type === "action" && (key === "actions" || key === "triggered_actions")) {
-            formData = {
-                ...formData,
-                [key]: [...(formData[key] as Action[]), sampleAction]
-            };
-        }
+        formData = updatedFormData;
     }
 
 
-    // We've created a custom submit function to pass the response and close the modal.
+
     function onFormSubmit(): void {
         if ($modalStore[0].response) $modalStore[0].response(formData);
         modalStore.close();
@@ -95,54 +102,132 @@
     const cBase = 'card p-4 w-modal shadow-xl space-y-4 max-h-screen overflow-auto';
     const cHeader = 'text-2xl font-bold';
     const cForm = 'border border-surface-500 p-4 space-y-4 rounded-container-token';
+
 </script>
 
-{#snippet traitForm(parent: Monster | Action, key: string)}
+{#snippet traitForm(parent: Monster | Action, key: string, actionKey: string, actionIndex: number | null)}
     {#if parent && parent[key]}
         {#each parent[key] as trait, i (i)}
+            <div class="variant-ghost-surface rounded-xl p-2">
+                <div>
             <label class="label">
                 <span>Trait Name</span>
                 <input class="input" bind:value={trait.name}
-                          placeholder='Name...'>
+                       placeholder='Name...'>
             </label>
             <label class="label">
                 <span>Trait Description</span>
                 <input class="input" bind:value={trait.desc}
-                          placeholder='Description...'>
+                       placeholder='Description...'>
             </label>
+                </div>
+                <div>
+                    <button class="btn" aria-label="delete-button" onclick={() => handleRemoveFormClick(key, actionKey, actionIndex, i)}><i class="fa-solid fa-trash"></i></button>
+                </div>
+            </div>
         {/each}
     {/if}
-    <button class="btn" onclick={() => handleAddFormClick(key, "trait")}>Add Trait</button>
+{/snippet}
+
+{#snippet actionForm(parent: Monster | Action, key: string)}
+    {#if parent && parent[key]}
+        {#each parent[key] as action, i (i)}
+            <div class="variant-ghost-surface rounded-xl p-2 flex justify-between">
+                <div>
+                    <label class="label">
+                        <span>Action Name</span>
+                        <input class="input" bind:value={action.name}
+                               placeholder='Name...'>
+                    </label>
+                    <label class="label">
+                        <span>Action Description</span>
+                        <input class="input" bind:value={action.desc}
+                               placeholder='Description...'>
+                    </label>
+                    <div class="m-2 ml-4 p-2 flex flex-col gap-2">
+                        {@render traitForm(action, "traits", true, i)}
+                    </div>
+
+                    <button class="btn" onclick={() => handleAddFormClick(key, true, i)}>Add Trait</button>
+                </div>
+                <div>
+                    <button class="btn" aria-label="delete-button" onclick={() => handleRemoveFormClick(key, false, i)}><i class="fa-solid fa-trash"></i></button>
+                </div>
+            </div>
+        {/each}
+    {/if}
 {/snippet}
 
 {#if $modalStore[0]}
     <div class="modal-example-form {cBase}">
-        <header class={cHeader}>Battle Name</header>
-        <article>Provide your battle name in the field below.</article>
+        <header class={cHeader}>Custom Monster</header>
+        <article>Provide stats for your custom monster in fields below.</article>
         <!-- Enable for debugging: -->
         <form class="modal-form {cForm}">
-            <label class="label">
-                <span>Name</span>
-                <input class="input" type="text" bind:value={formData.name} placeholder="Enter name..."/>
-            </label>
-            <!--{#if formData.actions}-->
-            <!--    {#each formData.actions as action, i (i)}-->
-            <!--        <label class="label">-->
-            <!--            <span>Action Name</span>-->
-            <!--            <textarea class="textarea" rows="1" bind:value={action.name}-->
-            <!--                      placeholder='Name...'></textarea>-->
-            <!--        </label>-->
-            <!--        <label class="label">-->
-            <!--            <span>Action Description</span>-->
-            <!--            <textarea class="textarea" rows="1" bind:value={formData.actions[i].desc}-->
-            <!--                      placeholder='Description...'></textarea>-->
-            <!--        </label>-->
-            <!--    {/each}-->
-            <!--{/if}-->
+            <div class="flex flex-col w-full gap-2">
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="flex flex-col gap-1">
+                        <label class="label">
+                            <span>Name</span>
+                            <input class="input" type="text" bind:value={formData.name} placeholder="Enter name..."/>
+                        </label>
+                        <label class="label">
+                            <span>Size</span>
+                            <input class="input" type="text" bind:value={formData.size} placeholder="Enter size..."/>
+                        </label>
+                        <label class="label">
+                            <span>Level</span>
+                            <input class="input" type="text" bind:value={formData.level} placeholder="Enter level..."/>
+                        </label>
+                        <label class="label">
+                            <span>Role</span>
+                            <input class="input" type="text" bind:value={formData.role} placeholder="Enter role..."/>
+                        </label>
+                        <label class="label">
+                            <span>Initiative</span>
+                            <input class="input" type="text" bind:value={formData.initiative}
+                                   placeholder="Enter initiative..."/>
+                        </label>
+                    </div>
+                    <div class="flex justify-around items-center text-xl variant-ghost-surface rounded-3xl grow">
+                        <div class="flex flex-col">
+                            <label class="label">
+                                <span>AC</span>
+                                <input class="input" type="text" bind:value={formData.ac} placeholder="Enter ac..."/>
+                            </label>
+                            <label class="label">
+                                <span>PD</span>
+                                <input class="input" type="text" bind:value={formData.pd} placeholder="Enter pd..."/>
+                            </label>
+                            <label class="label">
+                                <span>MD</span>
+                                <input class="input" type="text" bind:value={formData.md} placeholder="Enter md..."/>
+                            </label>
+                            <label class="label">
+                                <span>HP</span>
+                                <input class="input" type="text" bind:value={formData.hp} placeholder="Enter hp..."/>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="card p-2 flex flex-col gap-2">
+                    <span class="text-xl">Actions</span>
+                    {@render actionForm(formData, "actions")}
+                    <button class="btn" onclick={() => handleAddFormClick("actions")}>Add Action</button>
 
-            {@render traitForm(formData, "traits")}
+                    <span class="text-xl">Special triggers</span>
+                    {@render actionForm(formData, "triggered_actions")}
+                    <button class="btn" onclick={() => handleAddFormClick("triggered_actions")}>Add Action</button>
 
+                    <span class="text-xl">Traits</span>
+                    {@render traitForm(formData, "traits", false, null)}
+                    <button class="btn" onclick={() => handleAddFormClick("traits")}>Add Trait</button>
 
+                    <span class="text-xl">Nastier Specials</span>
+                    {@render traitForm(formData, "nastier_traits", false, null)}
+                    <button class="btn" onclick={() => handleAddFormClick("nastier_traits")}>Add Trait</button>
+                </div>
+            </div>
         </form>
 
         <!-- prettier-ignore -->
---
+++ src/lib/data/13th-Age-SRD/13th-Age-Index.md
@@ -0,0 +1,109 @@
+---
+aliases: [13th-Age-SRD Table of Contents]
+created: +002023-02-07 2316
+description:
+image:
+permalink:
+publish:
+tags: 13A
+updated: +002023-04-30 1238
+---
+
+## 13th Age System Reference Document
+
+### Table of Contents
+
+1. [13th Age SRD Map of Contents](13thAgeShare/Map-of-Contents)
+2. [13th Age Legal Notice](compendium/LICENSE)
+3. [Icons](compendium/Icons/Icons)
+4. [Character Rules](compendium/Character-Rules/Character-Rules)
+	- [Race](compendium/Character-Rules/Race)
+	- [Class](compendium/Character-Rules/Class)
+	- [Abilities](compendium/Character-Rules/Abilities)
+	- [Combat Stats](compendium/Character-Rules/Combat-Stats)
+	- [One Unique Thing](compendium/Character-Rules/One-Unique-Thing)
+	- [Icon Relationships](compendium/Character-Rules/Icon-Relationships)
+	- [Backgrounds and Skill Checks](compendium/Character-Rules/Backgrounds-and-Skill-Checks)
+	- [Feats](compendium/Character-Rules/Feats/Feats)
+	- [Gear](compendium/Character-Rules/Gear/Gear)
+5. [Races](compendium/Races/Races)
+	- [Human](compendium/Races/Human)
+	- [Dwarf](compendium/Races/Dwarf)
+	- [Dark Elf](compendium/Races/Dark-Elf)
+	- [High Elf](compendium/Races/High-Elf)
+	- [Wood Elf](compendium/Races/Wood-Elf)
+	- [Gnome](compendium/Races/Gnome)
+	- [Half Elf](compendium/Races/Half-Elf)
+	- [Halfling](compendium/Races/Halfling)
+	- [Half-Orc](compendium/Races/Half-Orc)
+	- [Dragonic/Dragonspawn](compendium/Races/Dragonic-Dragonspawn)
+	- [Holy One/Aasimar](Races/Holy%20One-Aasimar)
+	- [Forgeborn/Dwarf-forged](compendium/Races/Forgeborn-Dwarf-forged)
+	- [Tiefling/Demontouched](compendium/Races/Tiefling-Demontouched)
+6. [Classes](compendium/Classes/Classes)
+	- [Class Basics](compendium/Classes/Class-Basics)
+	- [Barbarian](compendium/Classes/Barbarian)
+	- [Bard](compendium/Classes/Bard)
+	- [Chaos Mage](compendium/Classes/Chaos-Mage)
+	- [Cleric](compendium/Classes/Cleric)
+	- [Commander](compendium/Classes/Commander)
+	- [Druid](compendium/Classes/Druid)
+	- [Fighter](compendium/Classes/Fighter)
+	- [Monk](compendium/Classes/Monk)
+	- [Necromancer](compendium/Classes/Necromancer)
+	- [Occultist](compendium/Classes/Occultist)
+	- [Paladin](compendium/Classes/Paladin)
+	- [Ranger](compendium/Classes/Ranger)
+	- [Rogue](compendium/Classes/Rogue)
+	- [Sorcerer](compendium/Classes/Sorcerer)
+	- [Wizard](compendium/Classes/Wizard)
+7. [Multiclassing](compendium/Multiclassing/Multiclassing)
+	- [Multiclassing Basics](compendium/Multiclassing/Multiclassing#Multiclassing%20Basics)
+	- [Key Ability Modifier](compendium/Multiclassing/Multiclassing#Key%20Ability%20Modifier)
+	- [Barbarian Multiclass](compendium/Multiclassing/Barbarian-Multiclass)
+	- [Bard Multiclass](compendium/Multiclassing/Bard-Multiclass)
+	- [Chaos Mage Multiclass](compendium/Multiclassing/Chaos-Mage-Multiclass)
+	- [Cleric Multiclass](compendium/Multiclassing/Cleric-Multiclass)
+	- [Commander Multiclass](compendium/Multiclassing/Commander-Multiclass)
+	- [Druid Multiclass](compendium/Multiclassing/Druid-Multiclass)
+	- [Fighter Multiclass](compendium/Multiclassing/Fighter-Multiclass)
+	- [Monk Multiclass](compendium/Multiclassing/Monk-Multiclass)
+	- [Necromancer Multiclass](compendium/Multiclassing/Necromancer-Multiclass)
+	- [Occultist Multiclass](compendium/Multiclassing/Occultist-Multiclass)
+	- [Paladin Multiclass](compendium/Multiclassing/Paladin-Multiclass)
+	- [Ranger Multiclass](compendium/Multiclassing/Ranger-Multiclass)
+	- [Rogue Multiclass](compendium/Multiclassing/Rogue-Multiclass)
+	- [Sorcerer Multiclass](compendium/Multiclassing/Sorcerer-Multiclass)
+	- [Wizard Multiclass](compendium/Multiclassing/Wizard-Multiclass)
+8. [Combat Rules](Combat%20Rules/Combat-Rules)
+	- Combat Stats
+	- Combat Sequence
+	- Combat Actions
+	- Special Actions
+	- Attacks
+	- Damage and Healing
+	- Death and Dying
+	- Rest and Recharge
+	- Combat Modifiers
+	- Special Attacks and Effects
+9. [Running the Game](compendium/Running-the-Game/Running-the-Game)
+	- [Using Icon Relationships](compendium/Running-the-Game/Running-the-Game#Using%20Icon%20Relationships)
+	- [Environments](compendium/Running-the-Game/Running-the-Game#Environments)
+	- [Building Battles](compendium/Running-the-Game/Building-Battles)
+	- [Full Heal-ups](compendium/Running-the-Game/Running-the-Game#Full%20Heal-ups)
+	- [Leveling Up](compendium/Running-the-Game/Leveling-Up)
+	- [Loot: Treasure Rewards](Running-the-Game/Loot-Treasure-Rewards)
+	- [Rituals](compendium/Running-the-Game/Rituals)
+10. [Monsters](compendium/Monsters/Monsters)
+	- [Monster Rules](Monsters/Monster-Rules)
+	- [Monster List](compendium/Monsters/Monster-List)
+	- [Monster Key](compendium/Monsters/Monster-Key)
+	- [Monster Creation](Monsters/Monster-Creation)
+	- [Underkrakens](compendium/Monsters/Underkrakens)
+11. [Magic Items](compendium/Magic-Items/Magic-Items)
+	- [One-Use Items](compendium/Magic-Items/One-Use-Items)
+	- [True Magic Items](compendium/Magic-Items/True-Magic-Items)
+	- [Chakras](compendium/Magic-Items/Chakras)
+	- [Magic Item Basics](compendium/Magic-Items/Magic-Item-Basics)
+	- [Artifacts](compendium/Magic-Items/Artifacts)
+	- [Magic Item Descriptions](Magic-Items/Magic-Item-Descriptions)
